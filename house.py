9f7a12ae4a85bc0050b1af2e9ea783f33d9683de
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-08 12:12:10 -0500

    Updated from Brython Server: 2/8/2018 12:12:09 PM

--- house.py
+++ house.py
@@ -1,3 +1,22 @@
 #Caleb Callaway
 #2/8/18
 #house.py - displays a house using ggame
+
+
+
+from ggame import *
+
+red = Color(0xFF0000,1)
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+black = Color(0X00000000,1)
+
+blackOutline = LineStyle(1,black)
+redRectangle = RectangleAsset(200,150, blackOutline, red) #(width,height,outline,fill)
+blackTriangle = PolygonAsset([(150,0),(0,150),(300,150)],blackOutline,black)#([relative vertex coordintates],etc)
+
+Sprite(redRectangle)
+Sprite(blackTriangle,(100,200))
+
+
+App().run()
