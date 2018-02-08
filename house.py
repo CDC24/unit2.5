ccb2fc14ed6b80ad7f1f1e33153614cfd6c7cdf6
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-08 12:15:38 -0500

    Updated from Brython Server: 2/8/2018 12:15:37 PM

--- house.py
+++ house.py
@@ -10,13 +10,15 @@ red = Color(0xFF0000,1)
 green = Color(0x00FF00,1)
 blue = Color(0x0000FF,1)
 black = Color(0X00000000,1)
+yellow = Color(0xFFFF00,1)
 
 blackOutline = LineStyle(1,black)
 redRectangle = RectangleAsset(200,150, blackOutline, red) #(width,height,outline,fill)
 blackTriangle = PolygonAsset([(150,0),(0,150),(300,150)],blackOutline,black)#([relative vertex coordintates],etc)
+yellowRectangle = RectangleAsset(10,10, blackOutline, yellow)
 
 Sprite(redRectangle,(150,350))
 Sprite(blackTriangle,(100,200))
-
+Sprite(yellowRectangle,(160,300))
 
 App().run()
