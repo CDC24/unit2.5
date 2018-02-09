6dd5a4530e0bfa864b59d79d356072b7ff856ea2
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-09 08:48:14 -0500

    Updated from Brython Server: 2/9/2018 8:48:13 AM

--- name.py
+++ name.py
@@ -1,3 +1,16 @@
 #Caleb Callaway
 #2/9/18
 #name.py - uses ggame to display name and backround RGB color
+
+
+
+from ggame import *
+
+RGB = input("Enter an RGB color code.")
+
+color = Color(0x(RGB),1)
+
+Sprite (Color)
+
+
+App().run()
