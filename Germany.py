7f0467aa2301d07ea717442c0a56270f24e3bc65
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-09 08:42:26 -0500

    Updated from Brython Server: 2/9/2018 8:42:25 AM

--- Germany.py
+++ Germany.py
@@ -15,8 +15,11 @@ blackRectangle = RectangleAsset(500,100, blackOutline, black)
 redRectangle = RectangleAsset(500,100, blackOutline, red)
 yellowRectangle = RectangleAsset(500,100, blackOutline, yellow)
 
+text = textAsset ("DEUTSCHLAAAAAAAAAAAAAAAAAAAAAAND!!!" fill = black style="bold 800 pt Times")
+
 Sprite (blackRectangle,(100,100))
 Sprite (redRectangle,(100,200))
 Sprite (yellowRectangle,(100,300))
+sprite (text, (100, 500)
 
 App().run()
