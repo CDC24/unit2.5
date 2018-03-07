0ed7ee5cee9df1ff97e0ceb1d6864399e57488c5
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-07 08:39:32 -0500

    Updated from Brython Server: 3/7/2018 8:39:32 AM

--- Israel.py
+++ Israel.py
@@ -1,3 +1,22 @@
 #Caleb Callaway
 #3/7/18
 #Israel.py - uses ggame to display Israeli flag
+
+
+from ggame import *
+
+black = Color(0X00000000,1)
+white = Color(0XFFFFFF,1)
+
+blackline = LineStyle(1,black)
+whiteline = LineStyle(1,white)
+
+backRectangle = RectangleAsset(1100,1000, blackline, black)
+whiteRectangle = RectangleAsset(500,300, whiteline, white)
+
+Sprite (backRectangle)
+Sprite (namewrite)
+
+
+
+App().run()
