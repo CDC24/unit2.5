dbbd9670bc5dcc4e4d32ba45ce4166fb2d7d6d30
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-07 08:26:04 -0500

    Updated from Brython Server: 3/7/2018 8:26:01 AM

--- name.py
+++ name.py
@@ -6,12 +6,16 @@
 
 from ggame import *
 
-RGB = input("Enter a 6-digit RGB color code.")
+name= input ("What's yer name? ")
+
+RGB = input("Enter a 6-digit RGB color code starting with 0x (ex. 0xFF0000) ")
+black = Color(0X00000000,1)
 
 theColor = Color((RGB),1)
 line = LineStyle(1,theColor)
 
-colorRectangle = RectangleAsset(900,900, line, theColor)
+colorRectangle = RectangleAsset(1100,1000, line, theColor)
+namewrite = TextAsset((name), fill=black, style="bold 400 pt Times")
 
 Sprite (colorRectangle)
 
