3e46fb7a9c33efdc258b8b65432b0f0f31fc8700
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-07 08:20:02 -0500

    Updated from Brython Server: 3/7/2018 8:20:01 AM

--- name.py
+++ name.py
@@ -8,9 +8,10 @@ from ggame import *
 
 RGB = input("Enter a 6-digit RGB color code.")
 
-Color = Color(0xRGB,1)
+theColor = Color((RGB),1)
+line = LineStyle(1,theColor)
 
-colorRectangle = RectangleAsset(900,900, Color)
+colorRectangle = RectangleAsset(900,900, line, theColor)
 
 Sprite (colorRectangle)
 
